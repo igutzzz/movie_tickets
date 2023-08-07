66ede1f44dc2192b7657d716f3599ce297cbe2bd
igutzzz <igor.almeida@madeinweb.app>
2023-08-07 15:53:17 -0300

    feat: change view to list

--- lib/data/movie_class.dart
+++ lib/data/movie_class.dart
@@ -2,19 +2,19 @@ class Movie {
   final bool adult;
   final String backdropPath;
   final int id;
-  final String title;
+  final String? title;
   final String overview;
-  final String posterPath;
-  final double rating;
+  final String? posterPath;
+  final double? rating;
 
   const Movie(
       {required this.adult,
       required this.backdropPath,
       required this.id,
-      required this.title,
+      this.title,
       required this.overview,
-      required this.posterPath,
-      required this.rating,});
+      this.posterPath,
+      this.rating,});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
@@ -22,9 +22,9 @@ class Movie {
         backdropPath: json['backdrop_path'],
         id: json['id'],
         overview: json['overview'],
-        posterPath: json['poster_path'],
-        rating: json['vote_average'],
-        title: json['title'],
+        posterPath: json['poster_path'] ?? '',
+        rating: json['vote_average'] ?? 0,
+        title: json['title'] ?? json['name'],
       );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Movie> movie;
+  late Future<List<Movie>> movie;
 
-  Future<Movie> fetchMovies() async {
+  Future<List<Movie>> fetchMovies() async {
     final response = await http.get(Uri.parse(
-        'https://api.themoviedb.org/3/trending/all/week?api_key=ed4c6ad6792ebf473ab3b183a8349992&language=pt-BR'));
+        'https://api.themoviedb.org/3/trending/movie/week?api_key=ed4c6ad6792ebf473ab3b183a8349992&language=pt-BR'));
 
     if (response.statusCode == 200) {
       var res = jsonDecode(response.body);
-      return Movie.fromJson(res['results'][0]);
+      List results = res['results'];
+      List<Movie> movies = [];
+      for (var item in results) {
+        movies.add(Movie(
+            adult: item['adult'],
+            backdropPath: item['backdrop_path'],
+            id: item['id'],
+            title: item['title'],
+            overview: item['overview'],
+            posterPath: item['poster_path'],
+            rating: item['vote_average']));
+      }
+
+      return movies;
     } else {
       throw Exception('Failed to load movies');
     }
@@ -41,28 +54,56 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FutureBuilder<Movie>(
-        future: movie,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return MovieItem(
-                id: snapshot.data!.id,
-                title: snapshot.data!.title,
-                overview: snapshot.data!.overview,
-                poster: snapshot.data?.posterPath ?? "",
-                rating: snapshot.data!.rating,
-              );
-          } else if (snapshot.hasError) {
-            print(snapshot.error);
-            print(snapshot.stackTrace);
-            return const Text(
-              'Erro ao acessar os dados',
-              textDirection: TextDirection.ltr,
-            );
-          }
-          return const CircularProgressIndicator();
-        },
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: double.maxFinite,
+            child: Column(
+              children: [
+                const Text("Encontre ingressos"),
+                Expanded(
+                  child: FutureBuilder<List<Movie>>(
+                    future: movie,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData &&
+                          snapshot.connectionState == ConnectionState.done) {
+                        return GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 20,
+                              childAspectRatio: 20/29
+                            ),
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: snapshot.data?.length,
+                          padding: const EdgeInsets.only(left: 20, right: 20),
+                          itemBuilder: (context, index) {
+                            return MovieItem(
+                                id: snapshot.data![index].id,
+                                title: snapshot.data![index]?.title ?? '',
+                                overview: snapshot.data![index].overview,
+                                poster: snapshot.data?[index].posterPath ?? '',
+                                rating: snapshot.data?[index].rating ?? 0);
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        print(snapshot.error);
+                        print(snapshot.stackTrace);
+                        return const Text(
+                          'Erro ao acessar os dados',
+                          textDirection: TextDirection.ltr,
+                        );
+                      }
+                      return const CircularProgressIndicator();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
